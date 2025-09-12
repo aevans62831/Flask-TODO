345bb96f7d87484233fa325178711b2ee5078966
Lex E. <aevans@mailbox.org>
2025-09-12 12:32:36 -0500

    We'll call this V1, sure.

--- app.py
+++ app.py
@@ -1,43 +1,46 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from models import *
 app = Flask(__name__)
-app.secret_key = ""
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db.init_app(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
 
-@app.route('/user/<name>')
-def hello_user(name):
-    return render_template('index.html', name=name)
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    new_todo = Todo(task=task)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.get(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/edit/<int:todo_id>', methods=['GET', 'POST'])
+def edit(todo_id):
+    todo = Todo.query.get(todo_id)
     if request.method == 'POST':
-        username = request.form['username']
-        session['username'] = username
-        return redirect(url_for('profile'))
-    return '''
-        <form method="post">
-            <p><input type=text name=username></p>
-            <p><input type=submit value=Login></p>
-        </form>
-    '''
+        todo.task = request.form.get('task')
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', todo=todo)
 
-@app.route('/profile')
-def profile():
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in.'
-
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('login'))
-
+@app.route('/toggle_complete/<int:todo_id>', methods=['POST'])
+def toggle_complete(todo_id):
+    todo = Todo.query.get(todo_id)
+    todo.completed = not todo.completed
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run()
--- models.py
+++ models.py
@@ -10,3 +10,9 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+
